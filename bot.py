c2a2db629a11ddebedb56f9e1d719052c623490b
caycothucuanoibuon <trungbung166@gmail.com>
2023-08-12 11:47:29 +0700

    update

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+import responses
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else message.channel.send(response)
+    except Exception as e:
+        print(e)
+def run_discord_bot():
+    TOKEN = 'MTEzOTE4MDgyNjYxODI0OTI4OA.GXS8mh.d9DwkVWuN6GAQAGKwNU1v1S1yGs5AKOxMqzeos'
+    client = discord.Client(intents=discord.Intents.default())
+    
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is running!')
+
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
---
+++ main.py
@@ -0,0 +1,5 @@
+import bot
+#script
+if __name__ == '__main__':
+    #run the bot
+    bot.run_discord_bot() 
